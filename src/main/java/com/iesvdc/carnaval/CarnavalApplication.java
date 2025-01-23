60b972e1ac36f026122d76a802356cc72c277e8e
anazeuqzav <anazeuqzav@gmail.com>
2025-01-23 16:12:05 +0100

    Initial commit

---
+++ src/main/java/com/iesvdc/carnaval/CarnavalApplication.java
@@ -0,0 +1,13 @@
+package com.iesvdc.carnaval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarnavalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarnavalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iesvdc/carnaval/CarnavalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iesvdc.carnaval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarnavalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
